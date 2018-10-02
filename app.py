8e43502d6e7941ba8d4d50e6192952f5bca5e5a0
Rizki Rusdiwijaya <rizki.rusdiwijaya@gmail.com>
2018-10-02 22:45:34 +0700

    flask simple rest

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'Hello, World!'}
+
+class Multi(Resource):
+    def get(self, num):
+        return {'result': num*10}
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(Multi, '/multi/<int:num>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
